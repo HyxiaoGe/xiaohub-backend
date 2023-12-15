ce3d9529a916ab0f5f6a7d149f10b48afd1b2c17
HyxiaoGe <18889592303@163.com>
2023-12-15 13:56:35 +0800

    fitst commit

---
+++ src/main/java/com/hyxiao/App.java
@@ -0,0 +1,13 @@
+package com.hyxiao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
