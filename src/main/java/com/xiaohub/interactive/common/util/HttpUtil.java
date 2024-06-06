d303e9e1203b3ed347d0c0fa99f216048578853a
HyxiaoGe <18889592303@163.com>
2024-06-06 17:28:16 +0800

    add retry strategy

--- src/main/java/com/xiaohub/interactive/common/util/HttpUtil.java
+++ src/main/java/com/xiaohub/interactive/common/util/HttpUtil.java
@@ -1,6 +1,7 @@
 package com.xiaohub.interactive.common.util;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.TrustStrategy;
@@ -9,40 +10,63 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContexts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.security.cert.X509Certificate;
 
 public class HttpUtil {
 
-    // 创建一个忽略所有证书的 HttpClient 实例
     private static final CloseableHttpClient httpClient = createClientAcceptsUntrustedCerts();
+
+    public static final Logger log = LoggerFactory.getLogger(HttpUtil.class);
 
     private static CloseableHttpClient createClientAcceptsUntrustedCerts() {
         try {
-            // 信任自签名和所有主机名的策略
             TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
             SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
 
-            return HttpClients.custom().setSSLContext(sslContext).setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE) // 关闭主机名验证
-                    .build();
+            // 配置请求的超时设置
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5000).setSocketTimeout(5000).setConnectionRequestTimeout(5000).build();
+
+            return HttpClients.custom().setSSLContext(sslContext).setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE).setDefaultRequestConfig(requestConfig).build();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static HttpResponse requestOpenAI(String payload, String proxyUrl, String apiKeys) throws Exception {
-        //  创建请求
-        HttpPost httpPost = new HttpPost(proxyUrl);
-        httpPost.addHeader("Content-Type", "application/json");
-        httpPost.addHeader("Authorization", "Bearer " + apiKeys);
-        //  设置请求体
-        StringEntity stringEntity = new StringEntity(payload, "UTF-8");
-        stringEntity.setContentType(new BasicHeader("Content-Type", "application/json"));
-        httpPost.setEntity(stringEntity);
-        //  发送请求并返回响应
+    public static HttpResponse requestOpenAI(String payload, String proxyUrl, String apiKeys) {
 
-        return httpClient.execute(httpPost);
+        int attemptCount = 0;
+        int maxAttempt = 3;
+        while (attemptCount <= maxAttempt) {
+            try {
+                //  创建请求
+                HttpPost httpPost = new HttpPost(proxyUrl);
+                httpPost.addHeader("Content-Type", "application/json");
+                httpPost.addHeader("Authorization", "Bearer " + apiKeys);
+                //  设置请求体
+                StringEntity stringEntity = new StringEntity(payload, "UTF-8");
+                stringEntity.setContentType(new BasicHeader("Content-Type", "application/json"));
+                httpPost.setEntity(stringEntity);
+                //  发送请求并返回响应
+                return httpClient.execute(httpPost);
+            } catch (SocketTimeoutException e) {
+                if (attemptCount < maxAttempt) {
+                    attemptCount++;
+                    log.warn("请求超时，正在重试， 当前重试次数:{}/{}", attemptCount, maxAttempt);
+                } else {
+                    throw new RuntimeException(e);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return null;
     }
 
 }
